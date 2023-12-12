e38ef8c151f754e0ff403b6e2fb79c94417cd424
mistarson <yeowuli2@naver.com>
2023-12-12 11:59:45 +0900

    Initial commit

---
+++ src/test/java/plus/plusassignment/PlusAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package plus.plusassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
